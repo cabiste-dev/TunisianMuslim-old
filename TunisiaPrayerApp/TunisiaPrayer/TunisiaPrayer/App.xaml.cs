597107843c9b32d2503eaa137a13176b72aad629
cabiste <ahmedjj354@gmail.com>
2022-01-23 22:23:27 +0100

    created project

---
+++ TunisiaPrayerApp/TunisiaPrayer/TunisiaPrayer/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TunisiaPrayer.Services;
+using TunisiaPrayer.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TunisiaPrayer
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TunisiaPrayerApp/TunisiaPrayer/TunisiaPrayer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TunisiaPrayer.ViewModels;
+using Xamarin.Forms;
+
+namespace TunisiaPrayer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
